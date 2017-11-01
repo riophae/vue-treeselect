20a9a667da95f312063639f9b52a1709791a9ec3
Fangzhou Li <riophaelee@gmail.com>
2017-11-01 11:17:10 +0800

    introduce stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: 'stylelint-config-xo-space',
+  rules: {
+    'string-quotes': [ 'double', { avoidEscape: false } ],
+    'declaration-empty-line-before': null,
+    'at-rule-empty-line-before': null,
+    'selector-list-comma-newline-after': null,
+    'rule-empty-line-before': null,
+    'value-keyword-case': null, // [ 'lower', { ignoreProperties: [ 'font', 'font-family' ] } ],
+    'declaration-block-no-duplicate-properties': [ true, { ignore: [ 'consecutive-duplicates' ] } ],
+    'declaration-property-value-blacklist': null,
+    'property-blacklist': null,
+    'no-unknown-animations': null,
+    'font-weight-notation': null,
+    'no-descending-specificity': null,
+  },
+}
