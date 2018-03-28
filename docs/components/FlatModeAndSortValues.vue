74602c520992a9512e6065b6b6de2f7006886fa5
Fangzhou Li <riophaelee@gmail.com>
2018-03-28 21:15:20 +0800

    update demo

--- docs/components/FlatModeAndSortValues.vue
+++ docs/components/FlatModeAndSortValues.vue
@@ -24,7 +24,7 @@
   export default {
     data() {
       return {
-        value: null,
+        value: [ 'c', 'aaa', 'bb' ],
         options: generateOptions(3),
         sortValueBy: 'ORDER_SELECTED',
       }
