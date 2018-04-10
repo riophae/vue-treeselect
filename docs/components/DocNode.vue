f8c82394aae4964e8a6c2d446e49d71e277ba9b9
Fangzhou Li <riophaelee@gmail.com>
2018-04-10 16:58:22 +0800

    improve consistency

--- docs/components/DocNode.vue
+++ docs/components/DocNode.vue
@@ -12,32 +12,32 @@
       <tbody>
         <tr>
           <td><strong>id</strong> (required)</td>
-          <td><code>Number</code> or <code>String</code></td>
+          <td>Number | String</td>
           <td>Used to identify the option within the tree. It's value must be unique across all options.</td>
         </tr>
         <tr>
           <td><strong>label</strong> (required)</td>
-          <td><code>String</code></td>
+          <td>String</td>
           <td>Used to display the option.</td>
         </tr>
         <tr>
           <td><strong>children</strong></td>
-          <td><code>Array&lt;node&gt;</code> or <code>null</code></td>
+          <td><code>node[]</code> | <code>null</code></td>
           <td>Declares a branch node. Set to empty array for no children options. Set to <code>null</code> for <a href="#delayed-loading">delayed loading</a>.</td>
         </tr>
         <tr>
           <td><strong>isBranch</strong></td>
-          <td><code>Boolean</code></td>
+          <td>Boolean</td>
           <td>Useful for declaring an <a href="#delayed-loading">unloaded branch node</a>. For other cases, just use <code>children</code> instead.</td>
         </tr>
         <tr>
           <td><strong>isDisabled</strong></td>
-          <td><code>Boolean</code></td>
+          <td>Boolean</td>
           <td>Used for disabling item selection. See <a href="#disable-item-selection">here</a> for detailed information.</td>
         </tr>
         <tr>
           <td><strong>isDefaultExpanded</strong></td>
-          <td><code>Boolean</code></td>
+          <td>Boolean</td>
           <td>Whether the branch option should be expanded by default.</td>
         </tr>
       </tbody>
