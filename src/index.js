7ec826fdcc07765da02064f69e8157bd281bb7d6
Fangzhou Li <riophaelee@gmail.com>
2018-07-05 05:48:34 +0800

    update comments

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './style.less'
 export default Treeselect
 export { Treeselect }
 export {
-  // delayed loading
+  // Delayed loading.
   LOAD_ROOT_OPTIONS,
   LOAD_CHILDREN_OPTIONS,
   ASYNC_SEARCH,
 } from './constants'
 
-// workaround that webpack doesn't set `__esModule=true` on library export
-// see: https://github.com/webpack/webpack/issues/2945#issuecomment-244733810
+// Workaround that webpack doesn't set `__esModule=true` on library export.
+// See: https://github.com/webpack/webpack/issues/2945#issuecomment-244733810
 export const __esModule = true
 export const VERSION = PKG_VERSION
