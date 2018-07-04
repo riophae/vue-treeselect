04e448b498d090a2edbd22f49dd73d19ec1140ce
Fangzhou Li <riophaelee@gmail.com>
2018-07-04 09:40:09 +0800

    v0.0.31

--- .size-limit.js
+++ .size-limit.js
@@ -1,4 +1,4 @@
 module.exports = [
-  { path: "dist/vue-treeselect.min.js", limit: "12 KB" },
+  { path: "dist/vue-treeselect.min.js", limit: "13 KB" },
   { path: "dist/vue-treeselect.min.css", limit: "7 KB" },
 ]
