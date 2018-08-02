097f493e2d1db7e946c3a1e006eb540420cf282a
Fangzhou Li <riophaelee@gmail.com>
2018-08-02 12:27:54 +0800

    add long label test

---
+++ docs/components/dev/LongLabelTest.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <treeselect :options="options" :multiple="multiple" v-model="value" />
+    <label><input type="checkbox" v-model="multiple"> Multi-select mode</label>
+  </div>
+</template>
+
+<script>
+  const longAToZ = 'abcdefghijklmnopqrstuvwxyz'.repeat(10)
+  const longZToA = longAToZ.split('').reverse().join('')
+
+  export default {
+    data: () => ({
+      multiple: true,
+      value: [ longAToZ ],
+      options: [ {
+        id: longAToZ,
+        label: longAToZ,
+      }, {
+        id: 'abc',
+        label: 'abc',
+      }, {
+        id: longZToA,
+        label: longZToA,
+      }, {
+        id: 'xyz',
+        label: 'xyz',
+      } ],
+    }),
+
+    watch: {
+      multiple(newValue) {
+        if (newValue) {
+          this.value = this.value ? [ this.value ] : null
+        } else {
+          this.value = this.value.length ? this.value[0] : null
+        }
+      },
+    },
+  }
+</script>
