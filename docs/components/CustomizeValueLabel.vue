f4e1a177025ca078aed56afaeece24f38cfc7641
BOSQUIER Gregory <bosqui_g@etna-alternance.net>
2018-08-08 14:38:33 +0200

    #75 customize selected value label (#140)

---
+++ docs/components/CustomizeValueLabel.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <treeselect
+    :options="options"
+    :value="value"
+    :multiple="multiple"
+    >
+    <div slot="value-label" slot-scope="{ node }">{{ node.raw.customLabel }}</div>
+  </treeselect>
+  <p>
+      <label><input type="checkbox" v-model="multiple">Multi-select</label>
+  </p>
+</div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      multiple: true,
+      value: null,
+      options: [ {
+        id: '1',
+        label: 'label1',
+        customLabel: 'custom label1',
+      },
+      {
+        id: '2',
+        label: 'label2',
+        customLabel: 'custom label2',
+      },
+      {
+        id: '3',
+        label: 'label3',
+        customLabel: 'custom label3',
+      } ]
+    }),
+  }
+</script>
--- src/components/SingleValue.vue
+++ src/components/SingleValue.vue
@@ -5,7 +5,16 @@
   export default {
     name: 'vue-treeselect--single-value',
     inject: [ 'instance' ],
+    methods: {
+      renderSingleValueLabel() {
+        const { instance, node } = this;
 
+        const customValueLabelRenderer = instance.$scopedSlots['value-label'];
+        return customValueLabelRenderer
+          ? customValueLabelRenderer({ node: instance.selectedNodes[0] })
+          : instance.selectedNodes[0].label;
+      },
+	  },
     render() {
       const { instance, $parent: { renderValueContainer } } = this
       const shouldShowValue = instance.hasValue && !instance.trigger.searchQuery
@@ -13,7 +22,7 @@
       return renderValueContainer([
         shouldShowValue && (
           <div class="vue-treeselect__single-value">
-            { instance.selectedNodes[0].label }
+            { this.renderSingleValueLabel() }
           </div>
         ),
         <Placeholder />,
